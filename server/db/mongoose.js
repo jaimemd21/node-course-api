79614422139c517cc275f780b8899211f781d8be
MitoCode <jaimemd21@gmail.com>
2017-06-28 00:01:24 -0500

    add update script

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://mitocode:123@ds135592.mlab.com:35592/todoapp_mitocode');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://mitocode:123@ds135592.mlab.com:35592/todoapp_mitocode');
 
 
 module.exports = { mongoose }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-var { ObjectID } = require('mongodb');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require('./models/todo');
@@ -24,8 +25,8 @@ app.post('/todos/', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({ todos })
+    Todo.find().then((todos) => {        
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     });
@@ -61,6 +62,32 @@ app.delete('/todos/:id', (req, res) => {
         }
 
         res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
     }).catch((e) => {
         res.status(400).send();
     });
